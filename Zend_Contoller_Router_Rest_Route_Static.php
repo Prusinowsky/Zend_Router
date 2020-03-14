f4e9f7ece288ae5a700e0091e53422d07ba40e99
Patryk Prusinowski <patryk181299@gmail.com>
2020-03-14 14:11:19 +0100

    Create Zend Router for Zend 1.12

---
+++ Zend_Contoller_Router_Rest_Route_Static.php
@@ -0,0 +1,18 @@
+<?php
+
+class Zend_Controller_Router_Rest_Route_Static extends Zend_Controller_Router_Route_Static {
+
+  protected $method;
+
+  public function __construct(
+       $method, $route, $defaults = array(), $reqs = array(), Zend_Translate $translator = null, $locale = null
+  ) {
+    parent::__construct($route, $defaults, $reqs, $translator, $locale);
+    $this->method = $method;
+  }
+
+  public function match($path, $partial = false){
+    $request = new Zend_Controller_Request_Http();
+    return mb_strtolower($request->getMethod()) === mb_strtolower($this->method) ? parent::match($path, $partial) : false;
+  }
+}
